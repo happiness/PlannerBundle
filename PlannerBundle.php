ace46ed5ae81d1453bb8cd1125bbd60d4e5087e8
Peter Törnstrand <peter.tornstrand@happiness.se>
2026-09-24 12:22:57 +0200

    Remove outdated code and unused method in `PlannerBundle`

--- PlannerBundle.php
+++ PlannerBundle.php
@@ -2,13 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Kimai time-tracking app.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace KimaiPlugin\PlannerBundle;
 
 use App\Plugin\PluginInterface;
@@ -16,8 +9,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PlannerBundle extends Bundle implements PluginInterface
 {
-    public function getPath(): string
-    {
-        return __DIR__;
-    }
 }
